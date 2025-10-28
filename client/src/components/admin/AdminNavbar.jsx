a44bc8df81bb6a05ca36fd126a553aa5d363d4fc
Ezana <ezanamulugeta89@gmail.com>
2025-10-28 22:03:30 +0300

    Create AdminNavbar.jsx

---
+++ client/src/components/admin/AdminNavbar.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+import { Link } from 'react-router-dom'
+import { assets } from '../../assets/assets'
+
+const AdminNavbar = () => {
+    
+    return (
+        <div className='flex items-center justify-between px-6 md:px-10 h-26 border-b border-primary-dull/30'>
+           <Link to="/">
+           <img src={assets.logo} alt="logo" className='w-36 h-auto ml-200' />
+           
+           
+           </Link>
+        </div>
+    )
+}
+
+export default AdminNavbar
