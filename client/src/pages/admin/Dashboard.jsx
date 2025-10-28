adc3241a566cec8b494196f91ca4a7a6f8680c83
Ezana <ezanamulugeta89@gmail.com>
2025-10-28 23:42:23 +0300

    Update Dashboard.jsx

--- client/src/pages/admin/Dashboard.jsx
+++ client/src/pages/admin/Dashboard.jsx
@@ -1,4 +1,4 @@
-import { ChartLineIcon, CircleDollarSignIcon, PlayCircleIcon, UserIcon } from 'lucide-react';
+import { ChartLineIcon, CircleDollarSignIcon, PlayCircleIcon, StarIcon, UserIcon } from 'lucide-react';
 import React, { useEffect, useState } from 'react';
 import { dummyDashboardData } from '../../assets/assets';
 import Loading from '../../components/Loading';
@@ -6,7 +6,7 @@ import Title from './Title';
 import BlurCircle from '../../components/BlurCircle'; // make sure this exists
 
 const Dashboard = () => {
-  const currency = import.meta.env.VITE_CURENCY;
+  const currency = import.meta.env.VITE_CURENCY
 
   const [DashboardData, setDashboardData] = useState({
     totalBookings: 0,
@@ -18,7 +18,7 @@ const Dashboard = () => {
 
   const dashboardCards = [
     { title: 'Total Bookings', value: DashboardData.totalBookings || "0", icon: ChartLineIcon },
-    { title: 'Total Revenues', value: currency + DashboardData.totalRevenue || "0", icon: CircleDollarSignIcon },
+    { title: 'Total Revenues', value: currency + " " + DashboardData.totalRevenue || "0", icon: CircleDollarSignIcon },
     { title: 'Active Shows', value: DashboardData.activeShows.length || "0", icon: PlayCircleIcon },
     { title: 'Total Users', value: DashboardData.totalUser || "0", icon: UserIcon }
   ];
@@ -32,9 +32,8 @@ const Dashboard = () => {
     fetchDashboardData();
   }, []);
 
-  if (loading) return <Loading />;
-
-  return (
+  
+  return !loading ? (
     <>
       <Title text1="admin" text2="Century Cinema" />
 
@@ -58,8 +57,38 @@ const Dashboard = () => {
           })}
         </div>
       </div>
+
+      <p className='mt-10 text-lg font-medium'> Active Shows</p>
+      <div className="relative flex flex-wrap gap-6 mt-4 max-w-5xl">
+  <BlurCircle top="100px" left="-10%" />
+  {dummyDashboardData.activeShows.map((show) => (
+    <div
+      key={show._id}
+      className="w-56 rounded-lg overflow-hidden h-full pb-3 bg-primary/10 border border-primary/20
+                 hover:-translate-y-1 transition duration-300"
+    >
+      <img
+        src={show.movie.poster_path}
+        alt={show.movie.title}
+        className="h-60 w-full object-cover"
+      />
+      <p className="font-medium p-2 truncate">{show.movie.title}</p>
+      <div className="flex items-center justify-between px-2">
+        <p className="text-lg font-medium">
+          {currency} {show.showPrice}
+        </p>
+        <p className="flex items-center gap-1 text-sm text-gray-400 mt-1 pr-1">
+          <StarIcon className="w-4 h-4 text-primary fill-primary" />
+          {show.movie.vote_average.toFixed(1)}
+        </p>
+      </div>
+    </div>
+  ))}
+</div>
+
+
     </>
-  );
+  ) : <Loading />
 };
 
 export default Dashboard;
