f428ee5d4fc15562cb9e531276f5425cb04abd4e
Ezana <ezanamulugeta89@gmail.com>
2025-11-04 20:13:38 +0300

    Update server.js

--- server/server.js
+++ server/server.js
@@ -1,10 +1,16 @@
 import express from 'express';
 import cors from 'cors';
 import 'dotenv/config';
+import connectDB from './configs/db.js';
+import { serve } from "inngest/express";
+import { inngest, functions } from "./inngest/index.js"
+import { clerkMiddleware } from '@clerk/express'
 
 
 const app = express();
 const port = 3000;
+
+await connectDB()
 
 
 //middleware
@@ -13,8 +19,12 @@ app.use(express.json())
 
 app.use(cors())
 
+app.use(clerkMiddleware())
+
+
 //Api Routes
 
 app.get('/', (req, res)=> res.send('Server is Live'))
+app.use('/api/inggest',  serve({ client: inngest, functions }))
 
 app.listen(port, ()=> console.log(  `Server listening at http://localhost:${port}`));
