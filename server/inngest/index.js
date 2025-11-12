85b540e3626d94a678b8f63085dc8d53c4c5938f
Ezana Mulugeta <Ezanamulugeta89@gmail.com>
2025-11-12 23:03:00 +0300

    Update index.js

--- server/inngest/index.js
+++ server/inngest/index.js
@@ -9,28 +9,29 @@ export const inngest = new Inngest({ id: "movie-ticket-booking" });
 
 
 
-const synncUserCreation = inngest.createFunction(
-    {
-        id: 'sync-user-from-clerk'
-    },
-    {
-        event: 'clerk/user.created'
-    },
-    async ({ event })=>{
-        const {
-            id,first_name, last_name, email_addresses, image_url
-        } = event.data
+const syncUserCreation = inngest.createFunction(
+  { id: "sync-user-from-clerk" },
+  { event: "clerk/user.created" },
+  async ({ event }) => {
+    const {
+      id,
+      first_name,
+      last_name,
+      email_addresses,
+      image_url,
+    } = event.data;
 
     const userData = {
-        _id: id,
-        email: email_addresses[0].email_addresses,
-        name: first_name + ' ' +last_name,
-        image: image_url
-    }
-    await User.create(userData)
+      _id: id,
+      email: email_addresses[0].email_address, // ✅ fixed key name
+      name: `${first_name || ""} ${last_name || ""}`.trim(),
+      image: image_url,
+    };
 
-    }
-)
+    await User.create(userData);
+  }
+);
+
 
 
 
@@ -71,7 +72,7 @@ const synncUserUpdation = inngest.createFunction(
 )
 // Create an empty array where we'll export future Inngest functions
 export const functions = [
-    synncUserCreation, 
+    syncUserCreation, 
     synncUserDeletion,
     synncUserUpdation
 ];
