f1e42594044f1f95ce3512c27217016aa6671b5d
Ezana Mulugeta <Ezanamulugeta89@gmail.com>
2025-11-15 23:38:35 +0300

    Update showRouter.js

--- server/Routes/showRouter.js
+++ server/Routes/showRouter.js
@@ -1,11 +1,13 @@
 import  express  from 'express';
-import { getNowPlayingMovies } from '../controllers/showControllers.js';
+import { addShow, getNowPlayingMovies, getShow } from '../controllers/showControllers.js';
+import { protectAdmin } from '../middleware/auth.js';
 
 const showRouter = express.Router();
-showRouter.get('/now-playing', getNowPlayingMovies) 
-
+showRouter.get('/now-playing',protectAdmin, getNowPlayingMovies) 
+showRouter.post('/add',protectAdmin, addShow)
+showRouter.get("/all", getShow)
+showRouter.get("/:movieId", getShow)
 
 export default showRouter;
 
 
-//http://localhost:3000/api/show/now-playing
