7f5f00d47819f9f265128285c9d44d3f0e1ac1fa
Ezana Mulugeta <Ezanamulugeta89@gmail.com>
2025-11-15 23:55:44 +0300

    fix the error

--- server/controllers/showControllers.js
+++ server/controllers/showControllers.js
@@ -119,7 +119,7 @@ export const addShow = async (req, res) =>{
 
 export const getShows = async (req, res) =>{
     try{
-        const shows = await (await Show.find({showDateTime: {$gte: new Date()}}).populate('movie')).toSorted({ showDateTime: 1});
+        const shows = await Show.find({showDateTime: {$gte: new Date()}}).populate('movie').sort({ showDateTime: 1});
 
 
         // filter unique shows 
