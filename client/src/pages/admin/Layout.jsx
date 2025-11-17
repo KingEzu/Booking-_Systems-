b8e3ead629fd02e276c8e70bbf19d5f4d015876c
Ezana Mulugeta <Ezanamulugeta89@gmail.com>
2025-11-17 22:38:51 +0300

    Update Layout.jsx

--- client/src/pages/admin/Layout.jsx
+++ client/src/pages/admin/Layout.jsx
@@ -1,12 +1,22 @@
-import React from 'react'
+import React, { use, useEffect } from 'react'
 import AdminNavbar from '../../components/admin/AdminNavbar'
 import AdminSidebar from '../../components/admin/AdminSidebar'
 import { Outlet } from 'react-router-dom'
+import { useAppContext } from '../../context/AppContext.jsx'
+import Loading from '../../components/Loading.jsx'
 
 const Layout = () => {
-    return (
+
+    const {isAdmin, fetchIsAdmin} = useAppContext()
+
+    useEffect(() => {
+        fetchIsAdmin()
+    },[])
+
+    
+    return isAdmin ? (
         <>
-            <AdminNavbar/>    
+            <AdminNavbar />    
             <div className='flex'>
                 <AdminSidebar/>
                 <div className='flex-1 px-4 py-10 md:px-10 h-[calc(100vh-64px)] overflow-y-auto'>
@@ -14,7 +24,7 @@ const Layout = () => {
                 </div>
             </div>
         </>
-    )
+    ) : <Loading />
 }
 
 export default Layout
