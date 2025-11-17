57bd8b290b21e3cdedfe76bb15ba3758ae18c7fc
Ezana Mulugeta <Ezanamulugeta89@gmail.com>
2025-11-17 22:38:33 +0300

    Update App.jsx

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -17,10 +17,15 @@ import AddShows from './pages/admin/AddShows.jsx'
 import ListShows from './pages/admin/ListShows.jsx'
 import ListBooking from './pages/admin/ListBooking.jsx'
 import ManageSnacks from './pages/admin/ManagSnacks.jsx'
+import { useAppContext } from './context/AppContext'
+import { SignIn } from '@clerk/clerk-react'
+
 
 const App = () => {
 
   const isAdminRoute = useLocation().pathname.startsWith('/admin');
+
+  const { user } = useAppContext()
   return (
     <>
     <Toaster/>
@@ -37,7 +42,12 @@ const App = () => {
         <Route path='/favorite' element={<Favorite/>}/>
         <Route path='/packages' element={<Packages />}/>
 
-      <Route path='/admin/*' element={<Layout/>}>
+      <Route path='/admin/*' element={user ? <Layout/> : (
+        <div className='min-h-screen flex justify-center items-center'>
+          <SignIn  fallbackRedirectUrl={'/admin'} />
+      
+      </div>
+      )}>
       <Route index element={<Dashboard/>}/>
       <Route path='add-shows' element={<AddShows/>}/>
       <Route path='list-shows' element={<ListShows/>}/>
