e321eb6936ea24ff32e80964ac8bd0298aab3bf6
Ezana Mulugeta <Ezanamulugeta89@gmail.com>
2025-11-19 22:30:13 +0300

    Update server.js

--- server/server.js
+++ server/server.js
@@ -6,6 +6,9 @@ import { serve } from "inngest/express";
 import { inngest, functions } from "./inngest/index.js"
 import { clerkMiddleware } from '@clerk/express'
 import showRouter from './Routes/showRouter.js';
+import bookingRouter from './Routes/bookingRouter.js';
+import adminRouter from './Routes/adminRoutes.js';
+import userRouter from './Routes/userRoutes.js';
 
 
 const app = express();
@@ -28,5 +31,8 @@ app.use(clerkMiddleware())
 app.get('/', (req, res)=> res.send('Server is Live'))
 app.use('/api/inggest',  serve({ client: inngest, functions }))
 app.use('/api/show', showRouter)
+app.use('/api/booking', bookingRouter)
+app.use('/api/admin', adminRouter)
+app.use('/api/user', userRouter)
 
 app.listen(port, ()=> console.log(  `Server listening at http://localhost:${port}`));
