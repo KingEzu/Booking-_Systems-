8218caa8da97fbbd0e62fa51578980f7facc7047
Ezana Mulugeta <Ezanamulugeta89@gmail.com>
2025-11-19 22:31:26 +0300

    Update main.jsx

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -7,21 +7,21 @@ import { BrowserRouter } from 'react-router-dom'
 import { AppProvider } from './context/AppContext'
 
  // Import your Publishable Key
-const PUBLISHABLE_KEY = import.meta.env.VITE_CLERK_PUBLISHABLE_KEY
+const PUBLISHABLE_KEY = import.meta.env.VITE_CLERK_PUBLISHABLE_KEY 
 
 if (!PUBLISHABLE_KEY) {
   throw new Error('Missing Publishable Key')
 }
 
 createRoot(document.getElementById('root')).render(
-    <ClerkProvider publishableKey={PUBLISHABLE_KEY} >
-   
+    <ClerkProvider publishableKey={PUBLISHABLE_KEY}>
       <BrowserRouter>
          <AppProvider>
-            <App />
-          </AppProvider>
-
+              <App />
+         </AppProvider>
+            
+          
       </BrowserRouter>
-    </ClerkProvider>
+    </ClerkProvider>,
   
 )
