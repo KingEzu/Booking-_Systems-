665d8e3c805324b175d7402917de18910a1b8f3a
Ezana Mulugeta <Ezanamulugeta89@gmail.com>
2025-11-22 22:37:49 +0300

    Update server.js

--- server/server.js
+++ server/server.js
@@ -4,10 +4,10 @@ import 'dotenv/config';
 import connectDB from './configs/db.js';
 import { serve } from "inngest/express";
 import { inngest, functions } from "./inngest/index.js"
-import { clerkMiddleware } from '@clerk/express'
+import { clerkMiddleware } from "@clerk/express";
 import showRouter from './Routes/showRouter.js';
 import bookingRouter from './Routes/bookingRouter.js';
-import adminRouter from './Routes/adminRoutes.js';
+import adminRouter from './Routes/adminRouter.js';
 import userRouter from './Routes/userRoutes.js';
 
 
@@ -23,7 +23,7 @@ app.use(express.json())
 
 app.use(cors())
 
-app.use(clerkMiddleware())
+app.use(clerkMiddleware());
 
 
 //Api Routes
@@ -32,7 +32,7 @@ app.get('/', (req, res)=> res.send('Server is Live'))
 app.use('/api/inggest',  serve({ client: inngest, functions }))
 app.use('/api/show', showRouter)
 app.use('/api/booking', bookingRouter)
-app.use('/api/admin', adminRouter)
+app.use("/api/admin", adminRouter)
 app.use('/api/user', userRouter)
 
 app.listen(port, ()=> console.log(  `Server listening at http://localhost:${port}`));
